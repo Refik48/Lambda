72ccca44af2793ccc2cc7cb6d92b97b585251318
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2021-09-09 20:34:07 +0100

    09.09.2021

--- src/streamOrnekler/Stream03Integer_Tekrar.java
+++ src/streamOrnekler/Stream03Integer_Tekrar.java
@@ -1,7 +1,10 @@
 package streamOrnekler;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Stream03Integer_Tekrar {
     public static void main(String[] args) {
@@ -12,6 +15,7 @@ public class Stream03Integer_Tekrar {
         liste.add(4);
         liste.add(9);
         liste.add(2);
+        liste.add(-2);
         liste.add(4);
         liste.add(12);
         liste.add(-1);
@@ -24,6 +28,12 @@ public class Stream03Integer_Tekrar {
         System.out.println("En BUYUK DEGER:" + kucukBul(liste));
         System.out.println("En BUYUK DEGER:" + carpimBul(liste));
         System.out.println("9 sayisi:" + dokuzSay(liste));
+        System.out.println("Negatif Liste:" + negatifSayiListesi(liste));
+        negatifSayilariYazdir(liste);
+        List<Integer> negatifler = negatifSayiListesi(liste);
+        System.out.println("\n negatif liste2" + negatifler);
+        System.out.println("\n Sayilarin Sirali Karesi" + tekKareSiraliListele(liste));
+
 
     }
     public static int buyukBul(List<Integer> liste){
@@ -35,7 +45,7 @@ public class Stream03Integer_Tekrar {
     public static int buyukBul2(List<Integer> liste){
         return liste.stream().sorted().reduce(0,(x,y)->y);
     }
-   public static int kucukBul(List<Integer> liste){
+    public static int kucukBul(List<Integer> liste){
         return liste.stream().sorted().reduce(0,(x,y)-> x<y ? x:y);
     }
     public static int carpimBul(List<Integer> liste){
@@ -45,5 +55,21 @@ public class Stream03Integer_Tekrar {
     public static int dokuzSay(List<Integer> liste){
         return (int) liste.stream().filter(x-> x==9).count();
     }
+    public static void negatifSayilariYazdir(List<Integer> liste){
+        liste.stream().filter(x-> x<0).forEach(System.out::print);
+    }
+   public static List<Integer> negatifSayiListesi(List<Integer> liste){
+        return liste.stream().filter(x->x<0).collect(Collectors.toList());
+   }
+   public static List<Integer> tekKareSiraliListele (List<Integer> liste){
+             return liste.
+                     stream().
+                     filter(Metotlar::tekMi).
+                     map(x ->(int) Math.pow(x,2)). // burada Math::pow kullanirsak dondurmeyi Liste<Double> yapmamiz gerekir
+                     distinct().                    // veya cast yapariz yani Math.pow()'un onune (int) yapariz
+                     sorted(). //sorted(Comparator.reverseOrder()).(Ters sirali yapmak istersek)
+                     collect(Collectors.toList());
+   }
+
 
 }
--- src/streamOrnekler/Stream04String_Tekrar.java
+++ src/streamOrnekler/Stream04String_Tekrar.java
@@ -1,4 +1,75 @@
 package streamOrnekler;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Stream04String_Tekrar {
+    public static void main(String[] args) {
+        List<String> liste = new ArrayList<>();
+        liste.add("Ali");
+        liste.add("Mark");
+        liste.add("Jackson");
+        liste.add("Amanda");
+        liste.add("alihano");
+        liste.add("Mariano");
+        liste.add("alberto");
+        liste.add("Alonzo");
+        liste.add("Tucker");
+        liste.add("Alfonso");
+        liste.add("Christ");
+        aIleBaslayanlar(liste);
+        System.out.println("================================");
+        buyukHarfeCevir(liste);
+        System.out.println("================================");
+        uzunlugaGoreSiralaKucult(liste);
+        System.out.println("================================");
+        uzunlugaGoreYazdir(liste,5);
+        System.out.println("================================");
+        System.out.println("Tum elemanlar belitilen degerden kucuk:" + uzunlukKucukMu(liste,9));
+        System.out.println("================================");
+        System.out.println("BASLAYAN YOK MU : " + baslamayanHarfVarMi(liste,"A"));
+        System.out.println("================================");
+        System.out.println("HERHANGI BITEN HARF VAR MI: " + herhangiBitenVarMi(liste,"c") );
+        System.out.println("================================");
+         aIleOYazdir(liste);
+        System.out.println("================================");
+    }
+    public static void aIleBaslayanlar(List<String> liste){
+        liste.stream().filter(h->h.startsWith("A")).forEach(System.out::println);
+    }
+   public static void buyukHarfeCevir(List<String> liste){
+       liste.stream().map(h -> h.toUpperCase()).forEach(System.out::println);
+    }
+    public static void uzunlugaGoreSiralaKucult (List<String> liste){
+        liste.stream().
+                sorted(Comparator.comparing(t -> t.length())).
+                map(t ->t.toLowerCase()).
+                forEach(System.out::println);
+    }
+    public static void uzunlugaGoreYazdir(List<String> liste , int uzunluk){
+        liste.stream().filter(t ->t.length() > uzunluk).forEach(System.out::println);
+    }
+    public static boolean uzunlukKucukMu (List<String> liste,int uzunluk){
+      return   liste.stream().allMatch(t -> t.length()< uzunluk);
+    }
+    public static boolean baslamayanHarfVarMi(List<String> liste,String harf){
+        return liste.stream().noneMatch(t->t.startsWith(harf));
+    }
+    public static boolean herhangiBitenVarMi(List<String> liste,String harf){
+       return liste.stream().anyMatch(t->t.endsWith(harf));
+    }
+    public static void aIleOYazdir (List<String> liste){
+        liste.stream().
+                filter(t->t.startsWith("A") && t.endsWith("o")).
+                forEach(System.out::println);
+    }
+    public static void listeleYazdir(List<String> liste){
+       liste.stream().sorted().map().forEach();
+
+    }
+
+
 }
+
+
